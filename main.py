f659cde49bb60bac19cb405326c781cefcb242f6
johnny98 <ishien98@gmail.com>
2020-02-17 15:12:41 +1100

    win streak

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# import pygame
 from tkinter import *
 from tkinter import ttk
-from random import randint
+from random import *
 from PIL import Image, ImageTk
+import time
+
 
 
 class RpsGame(Frame):
@@ -15,12 +16,19 @@ class RpsGame(Frame):
         self.master.resizable(False, False)#not able to resize the window size
         self.grid()
 
+        #winning count streak 
+        self.streak_count = Label(self, text=self.win_streak)
+
         # computer player shuffle image
-        self.image = PhotoImage(file="./images/rock.png")
-        self.imageLabel = Label(self, image=self.image)
+        self.computerimg = self.new_img()
+        self.shuffle_img = ImageTk.PhotoImage(Image.open(self.computerimg))
+        self.imageLabel = Label(self, image=self.shuffle_img)
         self.imageLabel.grid()
-        self.textLabel = Label(self, text="Choose option to play game!")
+        self.textLabel = Label(self, text="Start to play game \nChoose your option to win the game!")
         self.textLabel.grid()
+        # start button to update images 
+        self.start_button = Button(self, text="start", command=self.update_the_image)
+        self.start_button.grid()
 
         self.mid = Frame(root)
         self.mid.grid(pady=30, padx=30)
@@ -45,68 +53,71 @@ class RpsGame(Frame):
         self.s_btn = Button(self.mid, image=self.s_img, command="scissors")
         self.s_btn.grid(row=0, column=2, padx=10)
 
-    def new_img():
+    def new_img(self):
         #pick a new number
-        num = random.randit(0,2)
+        self.num = randint(0,2)
         #assigning img name to numbers
-        if num==0:
-            img = "rock"
-        elif num==1:
-            img="paper"
-        elif num==2:
-            img="scissors"
+        if self.num==0:
+            self.img = 'rock'
+        elif self.num==1:
+            self.img='paper'
+        elif self.num==2:
+            self.img='scissors'
         #return the imame name
-        computerimg = './images/'+img+'.png'
-
-        return computerimg
+        self.computerimg = './images/'+self.img+'.png'
+        return self.computerimg
         
-    def update_the_image():
-        computerimg = new_img()
-        updated_picture = ImageTk.PhotoImage(Image.open(myimg))
-        w.configure(image = updated_picture)
+    def update_the_image(self):
+        self.computerimg = self.new_img()
+        self.updated_picture = ImageTk.PhotoImage(Image.open(self.computerimg))
+        self.imageLabel.configure(image = self.updated_picture)
+            
 
-#game logic to add after wards 
-def game_logic():
-    # create a list of play options
-    play_options = ["rock", "paper", "scissors"]
+    def win_streak(self):
+        return 0
 
-    #assign a random play to the computer
-    computer = play_options[randint(0,2)]
+    #game logic to add after wards 
+    def game_logic(self):
+        # create a list of play options
+        play_options = ["rock", "paper", "scissors"]
 
-    #set player to False
-    player = False
-
-    while player == False:
-    #set player to True
-        player = input("Rock, Paper, Scissors? \n").lower()
-        if player == computer:
-            print("Tie!")
-        elif player == "rock":
-            if computer == "paper":
-                print("You lose!", computer, "covers", player)
-                break
-            else:
-                print("You win!", player, "smashes", computer)
-                break
-        elif player == "paper":
-            if computer == "scissors":
-                print("You lose!", computer, "cut", player)
-                break
-            else:
-                print("You win!", player, "covers", computer)
-                break
-        elif player == "scissors":
-            if computer == "rock":
-                print("You lose...", computer, "smashes", player)
-                break
-            else:
-                print("You win!", player, "cut", computer)
-                break
-        else:
-            print("That's not a valid play. Check your spelling!")
-        #player was set to True, but we want it to be False so the loop continues
-        player == False
+        #assign a random play to the computer
         computer = play_options[randint(0,2)]
+
+        #set player to False
+        player = False
+
+        while player == False:
+        #set player to True
+            player = input("Rock, Paper, Scissors? \n").lower()
+            if player == computer:
+                print("Tie!")
+            elif player == "rock":
+                if computer == "paper":
+                    print("You lose!", computer, "covers", player)
+                    break
+                else:
+                    print("You win!", player, "smashes", computer)
+                    break
+            elif player == "paper":
+                if computer == "scissors":
+                    print("You lose!", computer, "cut", player)
+                    break
+                else:
+                    print("You win!", player, "covers", computer)
+                    break
+            elif player == "scissors":
+                if computer == "rock":
+                    print("You lose...", computer, "smashes", player)
+                    break
+                else:
+                    print("You win!", player, "cut", computer)
+                    break
+            else:
+                print("That's not a valid play. Check your spelling!")
+            #player was set to True, but we want it to be False so the loop continues
+            player == False
+            computer = play_options[randint(0,2)]
 
 if __name__ == "__main__":
     RpsGame().mainloop()
--- test.py
+++ test.py
@@ -1,26 +1,33 @@
-from tkinter import *
-from tkinter import ttk
+# from tkinter import *
+# from tkinter import ttk
+# import time
 
-root = Tk()
-root.title("RPS GAME")
-root.geometry("300x300")
+# root = Tk()
+# root.title("RPS GAME")
+# root.geometry("300x300")
 
 
-l1=Label(root, text="Python", width=10, height=5, bg="red",padx=10, pady=10, relief="flat")
-l1.grid(column=0, row=0)
+# l1=Label(root, text="Python", width=10, height=5, bg="red",padx=10, pady=10, relief="flat")
+# l1.grid(column=0, row=0)
+# time.sleep(5)
 
-l2=Label(root, text="Python", width=10, height=5, bg="yellow",padx=10, pady=10, relief="groove")
-l2.grid(column=1, row=0)
+# l2=Label(root, text="Python", width=10, height=5, bg="yellow",padx=10, pady=10, relief="groove")
+# l2.grid(column=1, row=0)
 
-l3=Label(root, text="Python", width=10, height=5, bg="blue",padx=10, pady=10, relief="raised")
-l3.grid(column=2, row=0)
+# l3=Label(root, text="Python", width=10, height=5, bg="blue",padx=10, pady=10, relief="raised")
+# l3.grid(column=2, row=0)
 
-label4=Label(root, text="Python", width=10, height=5, bg="green",padx=10, pady=10, relief="ridge")
-label4.grid(column=3, row=0)
+# label4=Label(root, text="Python", width=10, height=5, bg="green",padx=10, pady=10, relief="ridge")
+# label4.grid(column=3, row=0)
 
-label5=Label(root, text="Python", width=10, height=5, bg="purple",padx=10, pady=10, relief="sunken")
-label5.grid(column=4, row=0)
+# label5=Label(root, text="Python", width=10, height=5, bg="purple",padx=10, pady=10, relief="sunken")
+# label5.grid(column=4, row=0)
 
-b1 = Button(root, text="ok",padx=30,pady=10,activebackground="blue")
-b1.grid(column=0,row=1)
-root.mainloop()
+# b1 = Button(root, text="ok",padx=30,pady=10,activebackground="blue")
+# b1.grid(column=0,row=1)
+# root.mainloop()
+
+import time 
+while True:
+    print("this prints once a second.")
+    time.sleep(0.1)
